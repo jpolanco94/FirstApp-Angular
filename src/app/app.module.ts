f445da2a9b7111e775e392847b9986fcb77e7343
Jorge Polanco <jorge.l.polancoAZ@outlook.com>
2019-12-04 19:55:15 -0800

    hey there

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './servers/server.component';
+import { SubServerComponent } from './sub-server/sub-server.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent
+    ServerComponent,
+    SubServerComponent
   ],
   imports: [
     BrowserModule,
